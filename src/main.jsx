957c38ec609235dc88cd4af44507a31bfdbe9427
GEdO23 <rm99632@fiap.com.br>
2023-09-13 11:40:39 -0300

    Adcionando página de erro ao router.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Error from './routes/Error.jsx'
 
 const router = createBrowserRouter([
-  { path: '/', element: <App />}
+  { path: '/', element: <App />, errorElement: <Error />}
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
